67e0bedaa19425d1f2adedf47dfedd2397e509f8
William Youmans <will@faithology.com>
2013-02-07 14:31:29 -0700

    customized clippy

--- clippy.hx
+++ clippy.hx
@@ -9,46 +9,46 @@ class Clippy {
   // Main
   static function main() {
     var text:String = flash.Lib.current.loaderInfo.parameters.text;
-    
+
     // label
-    
+
     var label:TextField = new TextField();
     var format:TextFormat = new TextFormat("Arial", 10);
-    
+
     label.text = "copy to clipboard";
     label.setTextFormat(format);
     label.textColor = 0x888888;
     label.selectable = false;
     label.x = 15;
     label.visible = false;
-    
+
     flash.Lib.current.addChild(label);
-    
+
     // button
-    
+
     var button:SimpleButton = new SimpleButton();
     button.useHandCursor = true;
     button.upState = flash.Lib.attach("button_up");
     button.overState = flash.Lib.attach("button_over");
     button.downState = flash.Lib.attach("button_down");
     button.hitTestState = flash.Lib.attach("button_down");
-    
+
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
       flash.system.System.setClipboard(text);
-      label.text = "copied!";
+      label.text = "";
       label.setTextFormat(format);
     });
-    
+
     button.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent) {
       label.visible = true;
     });
-    
+
     button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
       label.visible = false;
-      label.text = "copy to clipboard";
+      label.text = "";
       label.setTextFormat(format);
     });
-    
+
     flash.Lib.current.addChild(button);
   }
 }
